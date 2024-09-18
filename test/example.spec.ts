3e07258b805c5cabfccd4acb2e978a569e280aab
José Ben Hur <joseben.hur.86@gmail.com>
2024-09-18 17:34:11 -0300

    Criando primeiro teste

---
+++ test/example.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+
+test('o usuário consegue uma nova transação', () => {
+  // fazer a chamada HHTP p/ criar uma nova transação
+
+  const responseStatusCode = 201
+
+  expect(responseStatusCode).toEqual(201)
+  // validação
+})
