845e9f1e5bd82aabe682394b193d51b2a93317f3
José Ben Hur <joseben.hur.86@gmail.com>
2024-09-18 20:22:33 -0300

    Configurando banco de testes

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,7 @@
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
+import { execSync } from 'node:child_process'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -11,8 +12,13 @@ describe('Transactions routes', () => {
     await app.close()
   })
 
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
   it('should be able to create a new transaction', async () => {
-    const response = await request(app.server)
+    await request(app.server)
       .post('/transactions')
       .send({
         title: 'New Transaction',
@@ -31,7 +37,7 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
 
-    const cookies = createTransactionResponse.get('Set-Cookie')
+    let cookies = createTransactionResponse.get('Set-Cookie')
 
     const listTransactionsResponse = await request(app.server)
       .get('/transactions')
