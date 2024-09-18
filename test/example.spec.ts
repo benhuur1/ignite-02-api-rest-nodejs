7f5e8f00e6fbbc572eeac58f1ff45d6c3c1c00bf
José Ben Hur <joseben.hur.86@gmail.com>
2024-09-18 18:59:02 -0300

    Testando criação de transação
    Uma breve explicação sobre esses métodos:
    
        beforeAll
    
        É uma função que é executada uma única vez antes de todos os testes. É útil para inicializar recursos compartilhados que serão utilizados pelos testes.
    
        beforeEach
    
        É uma função que é executada antes de cada teste. É útil para preparar o ambiente antes da execução de cada teste, por exemplo, inicializar variáveis ou limpar o banco de dados.
    
        afterAll
    
        É uma função que é executada uma única vez após todos os testes terem sido executados. É útil para limpar recursos compartilhados ou fechar conexões abertas.
    
        afterEach
    
        É uma função que é executada após cada teste. É útil para limpar o ambiente depois da execução de cada teste, por exemplo, limpar variáveis ou fechar conexões com o banco de dados.

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,10 +1,21 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('o usuário consegue uma nova transação', () => {
-  // fazer a chamada HHTP p/ criar uma nova transação
+beforeAll(async () => {
+  await app.ready()
+})
 
-  const responseStatusCode = 201
-
-  expect(responseStatusCode).toEqual(201)
-  // validação
+afterAll(async () => {
+  await app.close()
+})
+test('user can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transaction',
+      amount: 500,
+      type: 'credit',
+    })
+    .expect(201)
 })
