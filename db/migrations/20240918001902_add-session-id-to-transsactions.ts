d57f39dfa3decd7caf507db99624755c7b11e799
José Ben Hur <joseben.hur.86@gmail.com>
2024-09-17 21:38:23 -0300

    Criando tabela de transações
    Seguindo o fluxo da aula anterior, vamos implementar os métodos up e down da migration que irá criar a tabela transactions

--- db/migrations/20240918001902_add-session-id-to-transsactions.ts
+++ db/migrations/20240918001902_add-session-id-to-transsactions.ts
@@ -9,5 +9,6 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transactions', (table) => {
     table.dropColumn('session_id')
+    
   })
 }
