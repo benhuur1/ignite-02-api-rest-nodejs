b1e9a9d413e6d357aa5809194df1c91ebc93d76d
José Ben Hur <joseben.hur.86@gmail.com>
2024-09-17 23:03:09 -0300

    Criação de transações
    Nessa aula, além de estruturar as rotas de transações, vamos aprender a validar os dados da requisição (request.body) com o Zod para garantir que as informações recebidas sejam válidas e após isso, fazer de fato a inserção no banco de dados.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,27 @@
-import { FastifyInstance } from "fastify"
-import { knex } from "../database"
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
 
-export async function transactionsRoutes(app: FastifyInstance){
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-    .where('amount', 500)
-    .select('*')
-  
-    return transactions
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    // 201
+    return reply.status(201).send()
   })
 }
